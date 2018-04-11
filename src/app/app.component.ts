9a929c0bb67650e97b350eb3a21802f00b8c8e8c
Verena Vertonghen <verena@Verenas-Mac-mini.local>
2018-04-11 11:19:24 +0200

    update design, readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [
+    `.demo-test-div {
+        height: 100px;
+        background-size: contain;
+        line-height: 100px;
+        text-align: center;
+        border-bottom: 1px solid grey;
+    }`
+  ]
 })
 export class AppComponent {
     title = 'app';
